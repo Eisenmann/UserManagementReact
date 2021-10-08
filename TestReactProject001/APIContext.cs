f7d6bab76a3a91578971f399db3440cee5495fd8
Eisenmann <tcheremnov@yandex.ru>
2021-10-08 18:50:05 +0300

    Initialize projects fix

---
+++ TestReactProject001/APIContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using TestReactProject001.Models;
+
+
+namespace TestReactProject001
+{
+    public class APIContext : DbContext
+    {
+        public DbSet<Users> Users {get;set;}
+    }
+}
